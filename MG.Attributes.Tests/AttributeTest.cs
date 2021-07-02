5f22f1ea19915532453e19aa17ea9136051418a0
Mike Garvey <mike.garvey@yevrag35.com>
2021-07-02 16:09:29 -0400

    Good tests

---
+++ MG.Attributes.Tests/AttributeTest.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using Moq;
+using Xunit;
+
+namespace MG.Attributes.Tests
+{
+    public class AttributeTest
+    {
+        private static readonly string[] _testStrings1 = new string[2]
+        {
+            "one", "two"
+        };
+        private static readonly string[] _testStrings2 = new string[2]
+        {
+            "three", "four"
+        };
+
+        [Fact]
+        public void GetAttributeValue()
+        {
+            var eval = new AttributeValuator();
+
+            int number = eval.GetAttributeValue<int, AdditionalValueAttribute>(Greetings.Hi);
+            Assert.Equal(123, number);
+        }
+
+        [Fact]
+        public void GetAttributeValues()
+        {
+            var eval = new AttributeValuator();
+
+            string[] strings = eval.GetAttributeValue<string[], AdditionalValueAttribute>(Greetings.Hello);
+            Assert.Equal(_testStrings1, strings);
+
+            string[] maybe = eval.GetAttributeValues<string, AdditionalValueAttribute>(Greetings.Hello).ToArray();
+            Assert.Equal(_testStrings1.Length, maybe.Length);
+            for (int i = 0; i < _testStrings1.Length; i++)
+            {
+                Assert.Equal(_testStrings1[i], maybe[i]);
+            }
+        }
+
+        [Fact]
+        public void GetComplexAttributeValues()
+        {
+            var eval = new AttributeValuator();
+
+            string[] strings = eval.GetAttributeValues<string, AdditionalValueAttribute>(Greetings.GoodMorning).ToArray();
+
+            Assert.Equal(_testStrings1.Length, strings.Length);
+            Assert.Equal(_testStrings1, strings);
+        }
+
+        [Fact]
+        public void GetMultipleArrays()
+        {
+            var eval = new AttributeValuator();
+            string[] staticArray = Enumerable.Concat(_testStrings1, _testStrings2).ToArray();
+
+            string[] strings = eval.GetAttributeValues<string, AdditionalValueAttribute>(Greetings.GoodAfternoon).ToArray();
+
+            Assert.Equal(staticArray.Length, strings.Length);
+            for (int i = 0; i < staticArray.Length; i++)
+            {
+                Assert.Equal(staticArray[i], strings[i]);
+            }
+        }
+
+        [Fact]
+        public void TryGetAttributeValue()
+        {
+            var eval = new AttributeValuator();
+
+            bool result = eval.TryGetAttributeValue<int, AdditionalValueAttribute>(Greetings.Hi, out int number, out Exception caughtException);
+            Assert.True(result);
+            Assert.Equal(123, number);
+            Assert.Null(caughtException);
+
+            bool result2 = eval.TryGetAttributeValue<string, AdditionalValueAttribute>(Greetings.Hello, out string another, out Exception caughtYa);
+            Assert.False(result2);
+            Assert.Null(another);
+            Assert.NotEqual(123.ToString(), another);
+            Assert.NotNull(caughtYa);
+        }
+    }
+}
---
+++ MG.Attributes.Tests/TestObjects.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MG.Attributes.Tests
+{
+    public class TestAttribute : Attribute, IValueAttribute
+    {
+        public virtual object Value { get; set; }
+
+        public virtual T GetAs<T>() => throw new NotImplementedException();
+        public virtual bool ValueIsString() => throw new NotImplementedException();
+
+        public TestAttribute(object value)
+            : base()
+        {
+            this.Value = value;
+        }
+    }
+    public enum Greetings
+    {
+        [AdditionalValue(123)]
+        Hi,
+
+        [AdditionalValue(new string[] { "one", "two" })]
+        Hello,
+
+        [AdditionalValue("one")]
+        [AdditionalValue("two")]
+        GoodMorning,
+
+        [AdditionalValue(new string[] { "one", "two" })]
+        [AdditionalValue(new string[] { "three", "four" })]
+        GoodAfternoon
+    }
+}
