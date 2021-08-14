554d0e66b8e96c5a5f952bc8f793f0fbfc89557f
Mike Garvey <mike.garvey@yevrag35.com>
2021-08-14 12:37:14 -0400

    Let's Go

---
+++ MG.Attributes-DotNet/Evaluator/AttributeValuator-Generic.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace MG.Attributes
+{
+    public partial class AttributeValuator<TAttribute, TOutput> where TAttribute : Attribute
+    {
+        private Func<TAttribute, TOutput> _singleValueFunction;
+        private Func<TAttribute, IEnumerable<TOutput>> _multipleValueFunction;
+
+        public AttributeValuator(Func<TAttribute, TOutput> valueFunction)
+        {
+            _singleValueFunction = valueFunction;
+            _multipleValueFunction = x => new TOutput[] { _singleValueFunction(x) };
+        }
+        public AttributeValuator(Func<TAttribute, TOutput> singleValueFunction, Func<TAttribute, IEnumerable<TOutput>> multiValueFunction)
+        {
+            _singleValueFunction = singleValueFunction;
+            _multipleValueFunction = multiValueFunction;
+        }
+
+        public TOutput GetValue(MemberInfo memberInfo)
+        {
+            ValidateArgument(memberInfo);
+
+            TAttribute att = memberInfo.GetCustomAttribute<TAttribute>();
+            if (null == att)
+                return default;
+
+            return _singleValueFunction(att);
+        }
+        public TOutput GetValue<TClass, TProp>(Expression<Func<TClass, TProp>> memberExpression)
+        {
+            if (TryAsMemberExpression(memberExpression, out MemberExpression memEx))
+            {
+                return this.GetValue(memEx.Member);
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public IEnumerable<TOutput> GetMultiValue(MemberInfo memberInfo)
+        {
+            ValidateArgument(memberInfo);
+
+            TAttribute att = memberInfo.GetCustomAttribute<TAttribute>();
+            if (null == att)
+                return default;
+
+            return _multipleValueFunction(att);
+        }
+        public IEnumerable<TOutput> GetMultiValue<TClass, TProp>(Expression<Func<TClass, TProp>> memberExpression)
+        {
+            if (TryAsMemberExpression(memberExpression, out MemberExpression memEx))
+            {
+                return this.GetMultiValue(memEx.Member);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<TOutput> GetMultiValues<TClass, TProp>(params Expression<Func<TClass, TProp>>[] expressions)
+        {
+            if (null == expressions || expressions.Length <= 0)
+                return null;
+
+            var list = new List<TOutput>(expressions.Length * 2);
+
+            for (int i = 0; i < expressions.Length; i++)
+            {
+                IEnumerable<TOutput> results = this.GetMultiValue(expressions[i]);
+                if (null != results)
+                    list.AddRange(results);
+            }
+
+            return list;
+        }
+        public IEnumerable<TOutput> GetValues<TMember>(IEnumerable<TMember> memberInfos)
+            where TMember : MemberInfo
+        {
+            ValidateArgument(memberInfos);
+
+            foreach (TMember info in memberInfos)
+            {
+                foreach (TAttribute tAtt in GetAttributes(info))
+                {
+                    foreach (TOutput value in _multipleValueFunction(tAtt))
+                    {
+                        yield return value;
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<TAttribute> GetAttributes(MemberInfo memberInfo)
+        {
+            foreach (TAttribute tAtt in memberInfo.GetCustomAttributes<TAttribute>())
+            {
+                yield return tAtt;
+            }
+        }
+        private static bool TryGetAttribute(MemberInfo memberInfo, out TAttribute propValueAtt)
+        {
+            propValueAtt = memberInfo.GetCustomAttributes<TAttribute>().FirstOrDefault();
+            return null != propValueAtt;
+        }
+        private static bool TryAsMemberExpression<TClass, TMember>(Expression<Func<TClass, TMember>> expression, out MemberExpression member)
+        {
+            member = null;
+
+            if (expression?.Body is MemberExpression memEx)
+            {
+                member = memEx;
+            }
+            else if (expression?.Body is UnaryExpression unEx && unEx.Operand is MemberExpression unExMem)
+            {
+                member = unExMem;
+            }
+
+            return null != member;
+        }
+        private static void ValidateArgument<T>(T argument)
+            where T : class
+        {
+            if (null == argument)
+            {
+                throw new ArgumentNullException(nameof(argument));
+            }
+        }
+        private static void ValidateArgument<T>(IEnumerable<T> argument)
+            where T : class
+        {
+            if (null == argument)
+            {
+                throw new ArgumentNullException(nameof(argument));
+            }
+        }
+    }
+}
---
+++ MG.Attributes-DotNet/Evaluator/AttributeValuator-Generic_Extended.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace MG.Attributes
+{
+    public partial class AttributeValuator<TAttribute, TOutput>
+    {
+        private const BindingFlags DEFAULT_FLAGS = BindingFlags.Public | BindingFlags.Instance;
+
+        public IEnumerable<TOutput> GetValues<TClass>(BindingFlags flags, bool includeFields = false)
+            where TClass : class
+        {
+            Type tClass = typeof(TClass);
+            IEnumerable<MemberInfo> members = tClass.GetProperties(flags);
+            if (includeFields)
+            {
+                members = members.Concat(tClass.GetFields(flags));
+            }
+
+            return this.GetValues(members);
+        }
+    }
+}
