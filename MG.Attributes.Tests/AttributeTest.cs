fb09f86b88f8920565f3bf6e67bf9d2806bacad5
Mike Garvey <mike.garvey@yevrag35.com>
2024-03-29 09:29:12 -0400

    Starts on v2

--- MG.Attributes.Tests/AttributeTest.cs
+++ MG.Attributes.Tests/AttributeTest.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Moq;
 using Xunit;
 
 namespace MG.Attributes.Tests
--- MG.Attributes.Tests/EnumTest.cs
+++ MG.Attributes.Tests/EnumTest.cs
@@ -1,5 +1,4 @@
-﻿using Moq;
-using Xunit;
+﻿using Xunit;
 using System;
 using System.Collections;
 using System.Collections.Generic;
